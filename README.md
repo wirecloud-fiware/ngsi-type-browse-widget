5188a918d0f7c89bc7c7fefda9d0f3a2157ba6ae
Álvaro Arranz <aarranz@fi.upm.es>
2015-07-21 19:38:18 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,93 @@
+NGSI Type Browser widget 
+========================
+
+The NGSI Type Browser widgets allows you to browser the entities types used in
+a given [Orion Context
+Broker](http://catalogue.fiware.org/enablers/publishsubscribe-context-broker-orion-context-broker)
+server in a easy and paginated way. This is done using the `contextTypes` querys, so
+updates made into the context broker are not reflected immediately when using
+this widget.
+
+> Latest version of this widget is always provided in [FIWARE
+> Lab](https://store.lab.fiware.org/search/keyword/OrionStarterKit) where you
+> can make use of it on the [Mashup portal](https://mashup.lab.fiware.org).
+> Remember to take a look into the example mashups provided in the OrionStarterKit offering.
+
+Build
+-----
+
+Be sure to have installed [Node.js](http://node.js). For example, you can install it on Ubuntu and Debian running the following commands:
+
+```bash
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install nodejs
+sudo apt-get install npm
+```
+
+Install other npm dependencies by running:
+
+```bash
+npm install
+```
+
+For build the widget you need download grunt:
+
+```bash
+sudo npm install -g grunt-cli
+```
+
+And now, you can use grunt:
+
+```bash
+grunt
+```
+
+If everything goes well, you will find a wgt file in the `dist` folder.
+
+Settings and Usage
+------------------
+
+### Settings
+
+- **NGSI server URL:** URL of the Orion Context Broker to use for retrieving
+  entity information.
+- **NGSI proxy URL:** URL of the Orion Context Broker proxy to use for receiving
+  notifications about changes.
+- **Use the FIWARE credentials of the user:** Use the FIWARE credentials of the
+  user logged into WireCloud. Take into account this option cannot be enabled if
+  you want to use this widget in a public workspace as anonoymous users doesn't
+  have a valid FIWARE auth token. As an alternative, you can make use of the
+  "Use the FIWARE credentials of the workspace owner" preference.
+- **Use the FIWARE credentials of the dashboard owner**: Use the FIWARE
+  credentials of the owner of the workspace. This preference takes preference
+  over "Use the FIWARE credentials of the user". This feature is available on
+  WireCloud 0.7.0+ in a experimental basis, future versions of WireCloud can
+  change the way to use it making this option not funcional and requiring you to
+  upgrade this operator.
+
+### Wiring
+
+##### Input Endpoints
+
+- This widget has no input endpoint
+
+##### Output Endpoints
+
+- This widget has no output endpoint
+
+Copyright and License
+---------------------
+
+Copyright (c) 2015 CoNWeT Lab., Universidad Politecnica de Madrid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/js/main.js
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2015 CoNWeT Lab., Universidad Politécnica de Madrid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* global NGSI, StyledElements */
+
+(function () {
+
+    "use strict";
+
+    var DataViewer = function DataViewer() {
+
+        /* Context */
+        MashupPlatform.widget.context.registerCallback(function (newValues) {
+            if (this.layout && ("heightInPixels" in newValues || "widthInPixels" in newValues)) {
+                this.layout.repaint();
+            }
+        }.bind(this));
+
+        /* Preferences */
+        MashupPlatform.prefs.registerCallback(this.updateNGSIConnection.bind(this));
+
+        this.layout = null;
+        this.table = null;
+    };
+
+    DataViewer.prototype.init = function init() {
+        createNGSISource.call(this);
+        this.updateNGSIConnection();
+
+        this.layout = new StyledElements.BorderLayout();
+        createTable.call(this);
+
+        this.layout.getCenterContainer().addClassName('loading');
+        this.layout.insertInto(document.body);
+        this.layout.repaint();
+    };
+
+    DataViewer.prototype.updateNGSIConnection = function updateNGSIConnection() {
+
+        this.ngsi_server = MashupPlatform.prefs.get('ngsi_server');
+        this.ngsi_proxy = MashupPlatform.prefs.get('ngsi_proxy');
+        this.ngsi_connection = new NGSI.Connection(this.ngsi_server, {
+            use_user_fiware_token: MashupPlatform.prefs.get('use_user_fiware_token'),
+            ngsi_proxy_url: this.ngsi_proxy
+        });
+    };
+
+    /**************************************************************************/
+    /****************************** HANDLERS **********************************/
+    /**************************************************************************/
+
+    var onRowClick = function onRowClick(row) {
+        //MashupPlatform.wiring.pushEvent('selected-row', row);
+    };
+
+    var onNGSIQuerySuccess = function onNGSIQuerySuccess(next, page, data, details) {
+        for (var i = 0; i < data.length; i++) {
+            data[i].attributes = data[i].attributes.join(', ');
+        }
+
+        var search_info = {
+            'resources': data,
+            'current_page': page,
+            'total_count': details.count
+        };
+
+        next(data, search_info);
+    };
+
+    var createNGSISource = function createNGSISource() {
+        this.ngsi_source = new StyledElements.PaginatedSource({
+            'pageSize': 20,
+            'requestFunc': function (page, options, onSuccess, onError) {
+                if (this.ngsi_connection !== null) {
+                    this.ngsi_connection.getAvailableTypes({
+                        limit: options.pageSize,
+                        offset: (page - 1) * options.pageSize,
+                        onSuccess: onNGSIQuerySuccess.bind(null, onSuccess, page),
+                        onFailure: onError
+                    });
+                } else {
+                    onSuccess([], {resources: [], total_count: 0, current_page: 0});
+                }
+            }.bind(this)
+        });
+        this.ngsi_source.addEventListener('requestStart', function () {
+            this.layout.getCenterContainer().disable();
+        }.bind(this));
+        this.ngsi_source.addEventListener('requestEnd', function () {
+            this.layout.getCenterContainer().enable();
+        }.bind(this));
+    };
+
+    var createTable = function createTable() {
+        var fields;
+
+        // Create the table
+        fields = [
+            {field: 'name', label: 'Type', sortable: true, width: "20%"},
+            {field: 'attributes', label: 'Attributes', sortable: false}
+        ];
+        this.table = new StyledElements.ModelTable(fields, {id: 'id', pageSize: 20, source: this.ngsi_source, 'class': 'table-striped'});
+        this.table.addEventListener("click", onRowClick);
+        this.table.reload();
+        this.layout.center.clear().appendChild(this.table);
+    };
+
+    var data_viewer = new DataViewer();
+    window.addEventListener("DOMContentLoaded", data_viewer.init.bind(data_viewer), false);
+
+})();
